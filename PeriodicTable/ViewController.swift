ea8b322897900ad56c527890b9fca74a38528be0
TakayukiABE <s1411344@coins.tsukuba.ac.jp>
2015-02-07 13:07:29 +0900

    Initial Commit

---
+++ PeriodicTable/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PeriodicTable
+//
+//  Created by takayuki abe on 2015/02/07.
+//  Copyright (c) 2015年 University of Tsukuba. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
